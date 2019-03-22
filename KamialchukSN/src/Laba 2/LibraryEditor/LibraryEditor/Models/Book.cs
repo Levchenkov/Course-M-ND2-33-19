99ad2f5bd1136940afce3c9d245838489c5c6707
Sergey Kamialchuk <sergeykamialchuk@gmail.com>
2019-03-22 21:07:16 +0300

    Laba_2
    Add_class

---
+++ KamialchukSN/src/Laba 2/LibraryEditor/LibraryEditor/Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace LibraryEditor.Models
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Author { get; set; }
+        [DataMember]
+        public DateTime Created { get; set; }
+        [DataMember]
+        public IEnumerable<EnumGanre> Ganre { get; set; }
+        [DataMember]
+        public bool IsPaper { get; set; }
+        [DataMember]
+        public IEnumerable<EnumLanguages> Languages { get; set; }
+        [DataMember]
+        public bool DeliveryRequired { get; set; }
+    }
+
+    public enum EnumGanre
+    {
+        Crime = 1,
+        Detective = 2,
+        Science = 4,
+        Fantasy = 8,
+        Romance = 16,
+        Other = 32
+    }
+
+    public enum EnumLanguages
+    {
+        Russian = 1,
+        English = 2,
+        France = 4
+    }
+}
---
+++ KamialchukSN/src/Laba 2/LibraryEditor/LibraryEditor/Models/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEditor.Models
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        void Add(T entity);
+        void Edit(T entity);
+        void Delete(int id);
+        void SaveChanges();
+    }
+}
