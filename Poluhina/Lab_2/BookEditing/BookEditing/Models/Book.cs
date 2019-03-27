1d6b1c29f03f3b07f7bbb3794e4daded2f1ceee4
yulia <polushka.yulia@gmail.com>
2019-03-27 23:50:04 +0300

    Poluhina_Laba2_Commit_27_03

---
+++ Poluhina/Lab_2/BookEditing/BookEditing/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace BookEditing.Models
+{
+    [DataContract]
+    public class Book
+    {
+        
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Author { get; set; }
+        [DataMember]
+        public DateTime Created { get; set; }
+
+        [DataMember]
+        public string Genre { get; set; }
+        [DataMember]
+        public bool IsPaper { get; set; }
+
+        [DataMember]
+        public List<string> Languages { get; set; }
+
+        [DataMember]
+        public bool DeliveryRequred { get; set; }
+
+    }
+}
