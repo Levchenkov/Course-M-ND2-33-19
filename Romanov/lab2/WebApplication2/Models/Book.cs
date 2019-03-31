f6dffa5bcb1ca50da26e83d419a9bcb03518d0f0
r0man0v-r0man <romanovmailbox@mail.ru>
2019-03-31 23:36:00 +0300

    lab2 add dell show

--- Romanov/lab2/WebApplication2/Models/Book.cs
+++ Romanov/lab2/WebApplication2/Models/Book.cs
@@ -5,9 +5,22 @@ using System.Web;
 
 namespace WebApplication2.Models
 {
+    public enum Genre
+    {
+        fantastic,
+        poem,
+        novel
+    }
     public class Book
     {
         public int BookId { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime Created { get; set; }
+        public Genre Genre { get; set; }
+        public bool IsPaper { get; set; }
+        public string[] Languages { get; set; }
+        public bool Delivery { get; set; }
     }
 }
