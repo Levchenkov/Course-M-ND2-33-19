cc554bba2c7cfc518fdbdc7617c7cb034662449a
r0man0v r0man <romanovmailbox@mail.ru>
2019-04-04 10:24:29 +0300

    Add book model

---
+++ Romanov/lab3/WebApplication3/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public enum Genre
+    {
+        Fantastic,
+        Novel,
+        Poem
+    }
+    public class Book
+    {
+        [Required]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        public Genre Genre { get; set; }
+        public bool IsPaper { get; set; }
+        [Required]
+        public String[] Languages { get; set; }
+        public bool DeliveryRequired { get; set; }
+    }
+}
