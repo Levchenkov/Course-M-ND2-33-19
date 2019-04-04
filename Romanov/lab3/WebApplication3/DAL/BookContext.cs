ddc0b34e5a1be0cc7fa18d6a4f2ca9c1cc670475
r0man0v r0man <romanovmailbox@mail.ru>
2019-04-04 14:04:20 +0300

    add DAL folder

---
+++ Romanov/lab3/WebApplication3/DAL/BookContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(): base("BookContext")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Romanov/lab3/WebApplication3/DAL/BookInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.DAL
+{
+    public class BookInitializer : DropCreateDatabaseIfModelChanges<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            var students = new List<Book>
+            {
+            new Book{
+                BookID = 0,
+                Author = "Romanov",
+                Created = DateTime.Now,
+                Description ="Test Entity Description",
+                Genre =Genre.Fantastic,
+                Title ="Entity test title",
+                IsPaper = true,
+                DeliveryRequired = false,
+                Languages = new string[]{"English","Russian"}
+            },
+            };
+
+            students.ForEach(s => context.Books.Add(s));
+            context.SaveChanges();
+            
+        }
+    }
+}
