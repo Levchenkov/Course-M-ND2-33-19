5451635c60d3054e0b8bf4a87555f12aab99fa07
r0man0v-r0man <romanovmailbox@mail.ru>
2019-04-09 08:56:45 +0300

    add tracking changes to json file

--- Romanov/lab3/WebApplication3/DAL/BookContext.cs
+++ Romanov/lab3/WebApplication3/DAL/BookContext.cs
@@ -5,15 +5,58 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 using WebApplication3.Models;
+using Newtonsoft.Json;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Data.Entity.Core.Objects;
 
 namespace WebApplication3.DAL
 {
     public class BookContext : DbContext
     {
+
         public DbSet<Book> Books { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Modified);
+
+            foreach (var item in modifiedEntities)
+            {
+                var itemType = ObjectContext.GetObjectType(item.Entity.GetType());
+                if (itemType == typeof(Book))
+                {
+                    var bookID = ((Book)item.Entity).BookID;
+                    var originalEntity = Set(itemType).AsNoTracking().Cast<Book>().First(x => x.BookID == bookID);
+                    Log log = new Log()
+                    {
+                        ID = bookID,
+                        DateChanged = DateTime.Now,
+                        OldValue = originalEntity,
+                        NewValue = (Book)item.Entity
+                    };
+                    File.AppendAllText(HttpContext.Current.Server.MapPath("~/App_Data/Changes.json"), JsonConvert.SerializeObject(log));
+
+
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
+
+
+    public class Log
+    {
+        public int ID { get; set; }
+        public DateTime DateChanged { get; set; }
+        public Book OldValue { get; set; }
+        public Book NewValue { get; set; }
+    }
+
 }
