5f6ce6e0a9a7526b0cd79fca911259f026b479f8
r0man0v-r0man <romanovmailbox@mail.ru>
2019-04-13 19:19:36 +0300

    init DB and add migration

---
+++ Romanov/lab4/lab4/DAL/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using lab4.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace lab4.DAL
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationContext() : base ("ApplicationConnection")
+        {
+
+        }
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
---
+++ Romanov/lab4/lab4/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab4.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public int CreatedById { get; set; }
+        public ApplicationUser CreatedByApplicationUser { get; set; }
+
+        public int UpdatedById { get; set; }
+        public ApplicationUser UpdatedByApplicationUser { get; set; }
+    }
+}
