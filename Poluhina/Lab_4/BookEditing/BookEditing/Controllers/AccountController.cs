93da500f78cda6f9d4593bc8e011a8148bc4edd7
yulia <polushka.yulia@gmail.com>
2019-04-13 15:02:54 +0300

    Commit_15_02

---
+++ Poluhina/Lab_4/BookEditing/BookEditing/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using BookEditing.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookEditing.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManagerProvider UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<UserManagerProvider>();
+            }
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(UserRegister model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { UserName = model.Email, Email = model.Email};
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Poluhina/Lab_4/BookEditing/BookEditing/Models/UserRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookEditing.Models
+{
+    public class UserRegister
+    {        
+            [Required]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Required]
+            [Compare("Password", ErrorMessage = "Passwords aren't the same")]
+            [DataType(DataType.Password)]
+            public string PasswordConfirm { get; set; }
+    }
+}
