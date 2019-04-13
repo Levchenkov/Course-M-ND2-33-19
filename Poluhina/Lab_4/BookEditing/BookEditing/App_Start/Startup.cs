6365b8d430e78936c514dec64c220cae673a75bd
yulia <polushka.yulia@gmail.com>
2019-04-13 12:53:33 +0300

    minimum_setting

---
+++ Poluhina/Lab_4/BookEditing/BookEditing/App_Start/Startup.cs
@@ -0,0 +1,30 @@
+﻿using BookEditing.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(BookEditing.App_Start.Startup))]
+
+namespace BookEditing.App_Start
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<UserContext>(UserContext.Create);
+            app.CreatePerOwinContext<UserManagerProvider>(UserManagerProvider.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
+}
---
+++ Poluhina/Lab_4/BookEditing/BookEditing/Models/UserManagerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookEditing.Models
+{
+    public class UserManagerProvider: UserManager<User>
+    {
+        public UserManagerProvider(IUserStore<User> store)
+           : base(store)
+        {
+        }
+        public static UserManagerProvider Create(IdentityFactoryOptions<UserManagerProvider> options,
+                                           IOwinContext context)
+        {
+            UserContext db = context.Get<UserContext>();
+            UserManagerProvider manager = new UserManagerProvider(new UserStore<User>(db));
+            return manager;
+        }
+    }
+}
