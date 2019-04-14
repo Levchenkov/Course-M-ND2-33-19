bf43236acfc76a21cb76fc10b189a306a98862ba
r0man0v-r0man <romanovmailbox@mail.ru>
2019-04-14 23:43:05 +0300

    some magic for create book

---
+++ Romanov/lab4/lab4/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using lab4.DAL;
+using lab4.Models;
+using lab4.Models.ViewModels;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace lab4.Controllers
+{
+   
+    public class BookController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+        private ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book model)
+        {
+            model.ApplicationUser = User.Identity.GetUserId();
+            return View();
+        }
+    }
+}
---
+++ Romanov/lab4/lab4/Models/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab4.Models.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
