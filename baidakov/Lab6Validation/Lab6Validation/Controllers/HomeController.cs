06f5a2e0016481c2a9d9bb20c9dfafe2bcdd723f
anatoly85 <47427834+anatoly85@users.noreply.github.com>
2019-04-23 20:17:11 +0300

    Add lab6

---
+++ baidakov/Lab6Validation/Lab6Validation/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Lab6Validation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation.Results;
+using System.Web.Mvc;
+using Lab6Validation.Validation;
+using System.Threading;
+
+namespace Lab6Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pay = new Payment()
+            {
+                FirstName = "Anatoly",
+                MiddleName = "Mihailovich",
+                LastName = "Baidakov",
+                Address = "Kalvariyskaya str., 43-1",
+                City = "Minsk",
+                Country = "Belarus",
+                PostCode = "21001",
+                Email = "am.baidakov@gmail.com",
+                Amount = 123.45,
+                Description = "test payment",
+                CreditCardNumber = "4111111111111111",
+                ExpirationMonth = 12,
+                ExpirationYear = 2021,
+                SecurityCode = "123"
+            };
+
+            return View(pay);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Payment pay)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Done");
+            }
+            else
+            {
+                return View(pay);
+            }
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            base.OnActionExecuting(filterContext);
+        }
+        public string Done()
+        {
+            return "Operation Completed Successfully";
+        }
+    }
+}
---
+++ baidakov/Lab6Validation/Lab6Validation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Lab6Validation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab6Validation")]
+[assembly: AssemblyCopyright("© , 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("8a982ec9-33ab-43c7-b143-94870a87f6bb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
